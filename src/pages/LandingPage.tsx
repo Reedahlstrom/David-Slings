47770d2c6aee67641b713ec550618a296b40a853
Reedahlstrom <reedahlstrom@gmail.com>
2026-09-09 00:23:32 -0600

    Simplify storefront hero to Go sling

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -45,11 +45,7 @@ export default function LandingPage() {
               <span className="tiny-sun">✳</span> MADE BY HAND. MEANT FOR
               OUTSIDE.
             </p>
-            <h1 id="hero-title">
-              Go throw
-              <br />
-              some rocks<span className="period">.</span>
-            </h1>
+            <h1 id="hero-title">Go sling</h1>
             <p className="hero-description">
               A leather pouch. Two cords. A really good
               <br className="desktop-break" /> reason to put your phone down.
